09cb110f40c22ae278d558cac3e81d8fef8eead9
Amin Mashayekhan <mashayekhan.mohammadamin@gmail.com>
2022-08-14 20:27:41 +0430

    Collidion checker funciton

--- src/gameHelpers.ts
+++ src/gameHelpers.ts
@@ -1,11 +1,41 @@
-import {STAGE_HEIGHT, STAGE_WIDTH} from './setup';
+import { STAGE_HEIGHT, STAGE_WIDTH } from './setup';
 
-import {TETROMINOS} from './setup';
+import { PLAYER } from './hooks/usePlayer';
+import { STAGE } from './components/Stage/Stage';
+import { TETROMINOS } from './setup';
 
 export const createStage = () => Array.from(Array(STAGE_HEIGHT), () => Array(STAGE_WIDTH).fill([0, 'clear']));
 
 export const randomTetromino = () => {
-    const tetrominos = ['I', 'J' , 'L', 'O', 'S', 'T', 'Z'] as (keyof typeof TETROMINOS)[];
+    const tetrominos = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'] as (keyof typeof TETROMINOS)[];
     const randTetromino = tetrominos[Math.floor(Math.random() * tetrominos.length)];
     return TETROMINOS[randTetromino];
+}
+
+export const isColliding = (
+    player: PLAYER,
+    stage: STAGE,
+    { x: moveX, y: moveY }: { x: number, y: number }
+) => {
+    // using for loops to be able to return (and break). Not possible with forEach
+    for (let y = 0; y < player.tetromino.length; y += 1) {
+        for (let x = 0; x < player.tetromino[y].length; x += 1) {
+            // 1. Ceck that we/re on an actual Tettromino cell
+            if (player.tetromino[y][x] !== 0) {
+                if (
+                    // 2. Check that our move is inside the game areas height (y)
+                    // That we're not moving trough the bottom of the grid
+                    !stage[y + player.pos.y + moveY] ||
+                    // 3. Check that our move is inside the game areas with (x)
+                    !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+                    // 4. Check that the cell we're moving to isn't set to clear
+                    stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== "clear"
+                ) {
+                    return true;
+                }
+            }
+        }
+    }
+    // 5. if everything above is false 
+    return false;
 }
