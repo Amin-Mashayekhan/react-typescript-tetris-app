810cc2a078246dc03399f769445f93378f233403
Amin Mashayekhan <mashayekhan.mohammadamin@gmail.com>
2022-08-29 20:17:56 +0430

    clear rows

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -9,9 +9,32 @@ export type STAGE = STAGECELL[][];
 
 export const useStage = (player: PLAYER, resetPlayer: () => void) => {
     const [stage, setStage] = React.useState(createStage());
+    const [rowsCleared, setRowsCleared] = React.useState(0);
 
     React.useEffect(() => {
         if (!player.pos) return;
+
+        setRowsCleared(0);
+
+
+        const sweepRows = (newStage: STAGE): STAGE => {
+            return newStage.reduce((ack, row) => {
+                // if we don't find a 0 it means that the row is full and should be cleared
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(prev => prev + 1);
+                    // Create an empty row at the beginning of the array to push the Tetrominos down
+                    // instead of returning the cleared row
+                    ack.unshift(new Array(newStage[0].length).fill([0, 'clear']) as STAGECELL[]);
+                    return ack;
+                }
+
+                ack.push(row);
+                return ack;
+
+
+            }, [] as STAGE)
+        }
+
 
         const updateStage = (prevStage: STAGE): STAGE => {
             // First flush the stage
@@ -23,14 +46,20 @@ export const useStage = (player: PLAYER, resetPlayer: () => void) => {
             // Then draw the tetromino
             player.tetromino.forEach((row, y) => {
                 row.forEach((value, x) => {
-                    if (value  !== 0) {
+                    if (value !== 0) {
                         newStage[y + player.pos.y][x + player.pos.x] = [value, `${player.collided ? "merged" : "clear"}`];
                     }
                 });
             });
+
+            if (player.collided) {
+                resetPlayer();
+                return sweepRows(newStage);
+            }
+
             return newStage;
         };
         setStage(prev => updateStage(prev));
     }, [player.collided, player.pos?.x, player.pos?.y, player.tetromino]);
-    return {stage, setStage}; 
+    return { stage, setStage };
 };
