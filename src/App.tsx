96f95b70c91ac441c4595b2cfefdef9e2cddfdff
Amin Mashayekhan <mashayekhan.mohammadamin@gmail.com>
2022-08-31 18:56:25 +0430

    complete game status feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { StyledTetris, StyledTetrisWrapper } from "./App.styles";
-import {createStage, isColliding} from './gameHelpers';
+import { createStage, isColliding } from './gameHelpers';
 
 import Display from './components/Display/Display';
 import React from "react";
 import Stage from './components/Stage/Stage';
 import StartButton from './components/StartButton/StartButton';
-import {useGameStatus} from './hooks/useGameStatus';
-import {useInterval} from './hooks/useInterval';
-import {usePlayer} from './hooks/usePlayer';
-import {useStage} from './hooks/useStage';
+import { useGameStatus } from './hooks/useGameStatus';
+import { useInterval } from './hooks/useInterval';
+import { usePlayer } from './hooks/usePlayer';
+import { useStage } from './hooks/useStage';
 
 const App: React.FC = () => {
   const [dropTime, setDropTime] = React.useState<null | number>(null);
@@ -16,21 +16,23 @@ const App: React.FC = () => {
 
   const gameArea = React.useRef<HTMLDivElement>(null);
 
-  const {player, updatePlayerPos, resetPlayer, playerRotate} = usePlayer();
-  const {stage, setStage, rowsCleared } = useStage(player, resetPlayer);
-  const {score, setScore, rows, setRows, level, setLevel} = useGameStatus(rowsCleared);
+  const { player, updatePlayerPos, resetPlayer, playerRotate } = usePlayer();
+  const { stage, setStage, rowsCleared } = useStage(player, resetPlayer);
+  const { score, setScore, rows, setRows, level, setLevel } = useGameStatus(rowsCleared);
 
   const movePlayer = (dir: number) => {
-    if (!isColliding(player, stage, {x: dir, y: 0})) {
-      updatePlayerPos({x: dir, y: 0, collided: false});
+    if (!isColliding(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0, collided: false });
     } // else we're not making any move
   };
 
-  const keyUp = ({keyCode}: {keyCode: number }): void => {
-    //  Change the dropTime speed when user releases down arrow
-    if(keyCode === 40) {
-      setDropTime(1000);
-      // setDropTime(1000 / level);
+  const keyUp = ({ keyCode }: { keyCode: number }): void => {
+    if (!gameOver) {
+      //  Change the dropTime speed when user releases down arrow
+      if (keyCode === 40) {
+        setDropTime(1000 / level + 200);
+        // setDropTime(1000 / level);
+      }
     }
   }
 
@@ -47,69 +49,71 @@ const App: React.FC = () => {
     setGameOver(false);
   }
 
-  const move =  ({keyCode, repeat}: {keyCode: number, repeat: boolean}): void => {
-    if (keyCode === 37) {
-      // this is going to be be the left arrow on keyboard
-      movePlayer(-1);
-    } else if (keyCode === 39){ 
-      // this is going to be be the right arrow on keyboard
-      movePlayer(1);
-    } else if (keyCode == 40) {
-      // this is going to be be the down arrow on keyboard
-      // Just call onces
-      if (repeat) return;
-      setDropTime(30);
-    } else if (keyCode === 38) {
-      // this is going to be be the up arrow on keyboard
-      // Implement this later  
-      playerRotate(stage);
+  const move = ({ keyCode, repeat }: { keyCode: number, repeat: boolean }): void => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        // this is going to be be the left arrow on keyboard
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        // this is going to be be the right arrow on keyboard
+        movePlayer(1);
+      } else if (keyCode == 40) {
+        // this is going to be be the down arrow on keyboard
+        // Just call onces
+        if (repeat) return;
+        setDropTime(30);
+      } else if (keyCode === 38) {
+        // this is going to be be the up arrow on keyboard
+        // Implement this later  
+        playerRotate(stage);
+      }
     }
   }
 
   const drop = (): void => {
     // Increase level when player has cleared 10 rows
-    if(rows > level * 10) {
+    if (rows > level * 10) {
       setLevel(prev => prev + 1);
-      // Also increase speed when player
-      setDropTime(1000/ level + 200);
+      // Also increase speed
+      setDropTime(1000 / level + 200);
     }
-    if (!isColliding(player, stage, {x: 0, y: 1})) {
-      updatePlayerPos({x: 0, y: 1, collided: false});
-    } else{ 
+    if (!isColliding(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
       // Game over!
       if (player.pos.y < 1) {
-         console.log("Game over!");
-         setGameOver(true);
-         setDropTime(null);
+        console.log("Game over!");
+        setGameOver(true);
+        setDropTime(null);
       }
-        updatePlayerPos({x: 0, y: 0, collided: true});
+      updatePlayerPos({ x: 0, y: 0, collided: true });
     }
   }
 
   useInterval(() => {
     drop();
   }, dropTime)
-  
+
   return (
     <StyledTetrisWrapper role='button' tabIndex={0} onKeyDown={move} onKeyUp={keyUp} ref={gameArea}>
-      <StyledTetris>  
-      <div className="display">
-        {gameOver ? (
-          <>
-            <Display gameOver={gameOver} text="Game Over!" />
-            <StartButton callback={handleStartGame} />
-          </>
-        ) : (
-          <>
-            <Display text={`Score: ${score}`} />
-            <Display text={`Rows: ${rows}`} />
-            <Display text={`Level: ${level}`} />
-      
-          </>
-        )
-      }
-      </div>
-      <Stage stage={stage} />
+      <StyledTetris>
+        <div className="display">
+          {gameOver ? (
+            <>
+              <Display gameOver={gameOver} text="Game Over!" />
+              <StartButton callback={handleStartGame} />
+            </>
+          ) : (
+            <>
+              <Display text={`Score: ${score}`} />
+              <Display text={`Rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
+
+            </>
+          )
+          }
+        </div>
+        <Stage stage={stage} />
       </StyledTetris>
     </StyledTetrisWrapper>
   );
